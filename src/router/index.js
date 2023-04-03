9d07b2db6631628cbc19bb35579a80ca7aca5ac9
tranhuy510 <huykslm99@gmail.com>
2023-04-04 01:03:50 +0700

    fix menu

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../pages/Root";
 import ForgotPassword from "../pages/ForgotPassword/ForgotPassword";
 import HomeDetail from "../pages/Details/HomeDetails";
+import Chat from "../pages/Chat";
 
 export const router = createBrowserRouter([
   {
@@ -19,6 +20,7 @@ export const router = createBrowserRouter([
         element: <Campaign />,
       },
       { path: "/detail", element: <HomeDetail /> },
+      { path: "/chat", element: <Chat /> },
       {
         path: "profile",
         element: <Profile />,
