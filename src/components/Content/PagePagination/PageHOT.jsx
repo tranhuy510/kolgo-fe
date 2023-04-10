84cca61dd4099b8d611832679e15b7efe33481f5
Zit biet code <haiconbo2020@gmail.com>
2023-04-10 21:38:55 +0700

    fixbug/register (#2)
    * fix register
    
    * fix verify account
    
    * message fixed - successmodal not - verify fix tam
    
    * fix re-render
    
    * update pass fix
    
    * done Authentication and Authorization
    
    * fix
    
    * code forgot password , verify email, change pass

--- src/components/Content/PagePagination/PageHOT.jsx
+++ src/components/Content/PagePagination/PageHOT.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useLayoutEffect } from 'react'
-import axios from 'axios';
 import styled from "styled-components";
+
+import { getKols } from '../../../services/authentication';
 
 const IMG = styled.img`
     width: 220px;
@@ -34,102 +35,40 @@ const DivWrap = styled.div`
 
 const PageHOT = (props) => {
 
-    const dataDemo1 = [
-        {
-            name: "thang",
-            src: "https://playerduo.com/api/upload-service/images/c5c6c610-c3e5-4bc8-a7f1-a6916fe5ca5e__710914a0-adc0-11ed-a19f-23a3b10d190e__player_avatar.jpg"
-        },
-        {
-            name: "thang",
-            src: "https://playerduo.com/api/upload-service/images/c5c6c610-c3e5-4bc8-a7f1-a6916fe5ca5e__710914a0-adc0-11ed-a19f-23a3b10d190e__player_avatar.jpg"
-        },
-        {
-            name: "thang",
-            src: "https://playerduo.com/api/upload-service/images/c5c6c610-c3e5-4bc8-a7f1-a6916fe5ca5e__710914a0-adc0-11ed-a19f-23a3b10d190e__player_avatar.jpg"
-        },
-        {
-            name: "thang",
-            src: "https://playerduo.com/api/upload-service/images/c5c6c610-c3e5-4bc8-a7f1-a6916fe5ca5e__710914a0-adc0-11ed-a19f-23a3b10d190e__player_avatar.jpg"
-        },
-        {
-            name: "thang",
-            src: "https://playerduo.com/api/upload-service/images/c5c6c610-c3e5-4bc8-a7f1-a6916fe5ca5e__710914a0-adc0-11ed-a19f-23a3b10d190e__player_avatar.jpg"
-        },
-    ]
-
-    const dataDemo2 = [
-        {
-            name: "2",
-            src: "https://playerduo.com/api/upload-service/images/b236f856-16c0-408a-ad6b-6eed82e2f366__763b7b90-8460-11ed-a19f-23a3b10d190e__player_avatar.jpg"
-        },
-        {
-            name: "2",
-            src: "https://playerduo.com/api/upload-service/images/b236f856-16c0-408a-ad6b-6eed82e2f366__763b7b90-8460-11ed-a19f-23a3b10d190e__player_avatar.jpg"
-        },
-        {
-            name: "2",
-            src: "https://playerduo.com/api/upload-service/images/b236f856-16c0-408a-ad6b-6eed82e2f366__763b7b90-8460-11ed-a19f-23a3b10d190e__player_avatar.jpg"
-        },
-        {
-            name: "2",
-            src: "https://playerduo.com/api/upload-service/images/b236f856-16c0-408a-ad6b-6eed82e2f366__763b7b90-8460-11ed-a19f-23a3b10d190e__player_avatar.jpg"
-        },
-        {
-            name: "2",
-            src: "https://playerduo.com/api/upload-service/images/b236f856-16c0-408a-ad6b-6eed82e2f366__763b7b90-8460-11ed-a19f-23a3b10d190e__player_avatar.jpg"
-        },
-    ]
-
-    const dataDemo3 = [
-        {
-            name: "3",
-            src: "https://playerduo.com/api/upload-service/images/20115ac4-b3d2-4a54-9f14-cda26c9eed30__e552ce90-ec79-11ec-92ac-1b8d2f5bc2b5__player_avatar.jpg"
-        },
-        {
-            name: "3",
-            src: "https://playerduo.com/api/upload-service/images/20115ac4-b3d2-4a54-9f14-cda26c9eed30__e552ce90-ec79-11ec-92ac-1b8d2f5bc2b5__player_avatar.jpg"
-        },
-        {
-            name: "3",
-            src: "https://playerduo.com/api/upload-service/images/20115ac4-b3d2-4a54-9f14-cda26c9eed30__e552ce90-ec79-11ec-92ac-1b8d2f5bc2b5__player_avatar.jpg"
-        },
-        {
-            name: "3",
-            src: "https://playerduo.com/api/upload-service/images/20115ac4-b3d2-4a54-9f14-cda26c9eed30__e552ce90-ec79-11ec-92ac-1b8d2f5bc2b5__player_avatar.jpg"
-        },
-        {
-            name: "3",
-            src: "https://playerduo.com/api/upload-service/images/20115ac4-b3d2-4a54-9f14-cda26c9eed30__e552ce90-ec79-11ec-92ac-1b8d2f5bc2b5__player_avatar.jpg"
-        },
-    ]
-
-    const data = [dataDemo1, dataDemo2, dataDemo3];
-    const [listKolHot, setListKolHot] = useState(data[props.current - 1])
+    const [listKolHot, setListKolHot] = useState([])
 
     useLayoutEffect(() => {
-        setListKolHot(data[props.current - 1]);
-    }, [props.current])
+        props.onChangeTotalHotKOL(listKolHot.length);
+    }, [listKolHot])
 
-    // useEffect(() => {
-    //     getData();
-    // }, [props.current])
+    useEffect(() => {
+        getKols()
+            .then(res => {
+                if (!res.ok) {
+                    return Promise.reject(res)
+                }
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                setListKolHot(data)
+            })
+    }, [])
 
-    // const getData = () => {
-    //     axios.get(`http://localhost:3000/page/${props.current}`)
-    //         .then((response) => {
-    //             if (response.status === 200) {
-    //                 setListKolHot(response.data)
-    //             };
-    //         });
-    // }
+    const onComeKolDetail = () => {
+        window.location.replace('http://localhost:3000/detail')
+    }
 
     return (
         <DivWrap>
-            {listKolHot?.map((item, index) => {
+            {listKolHot?.map((item) => {
                 return (
-                    <CardKOL>
-                        <IMG src={item.src} alt="" />
-                        <Name>{item.name}</Name>
+                    <CardKOL key={item.id} onClick={onComeKolDetail}>
+                        <IMG src={item?.ava} alt="" />
+                        <div style={{ display: 'flex' }}>
+                            <Name>{item.firstName}</Name>
+                            <Name>{item.lastName}</Name>
+                        </div>
                     </CardKOL>
                 )
             })}
