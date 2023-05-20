336bce988f478854de5f007a19adf861020e116d
Sang Do <svngdo@gmail.com>
2023-05-20 15:16:39 +0700

    update campaign service

--- src/services/CampaignService.js
+++ src/services/CampaignService.js
@@ -1,5 +1,21 @@
-import { postAuth } from "./Common";
+import { getAuth, postAuth, putAuth, deleteAuth } from "./Common";
 
 export function createCampaign(campaign) {
     return postAuth(`ent/campaigns`, campaign);
+}
+
+export function getCampaigns() {
+    return getAuth('ent/campaigns');
+}
+
+export function getCampaignByCampaignId(campaignId) {
+    return getAuth(`ent/campaigns/${campaignId}`);
+}
+
+export function updateCampaign(campaignId, campaign) {
+    return putAuth(`ent/campaigns/${campaignId}`, campaign);
+}
+
+export function deleteCampaign(campaignId) {
+    return deleteAuth(`ent/campaigns/${campaignId}`);
 }
