dfd50f2722350739d8dfa55f5efe417b07c8273c
- <haiconbo2020@gmail.com>
2023-05-25 04:06:25 +0700

    fix description

--- src/pages/Details/PageKolDetail/IntroduceKOL/IntroduceKOL.jsx
+++ src/pages/Details/PageKolDetail/IntroduceKOL/IntroduceKOL.jsx
@@ -4,9 +4,23 @@ const IntroduceKOL = (props) => {
     return (
         <div>
             <h1 style={{ opacity: '0.8' }}>Giới thiệu</h1>
-            <span style={{ whiteSpace: 'pre-wrap', lineHeight: '40px', fontSize: '20px', opacity: '0.8' }}>{props.description}</span>
+            <span style={{ fontSize: '20px', opacity: '0.8', whiteSpace: 'pre-wrap', }}>
+                <FormatText details={props.description} />
+            </span>
         </div>
     )
 }
+
+const FormatText = (props) => {
+    const formattedText = props.details?.replace(/\\n/g, '\n').split('\n').map((line, index) => {
+        return (
+            <React.Fragment key={index}>
+                {line}
+                <br /><br />
+            </React.Fragment>
+        );
+    });
+    return <div style={{ marginTop: '20px' }}>{formattedText}</div>;
+};
 
 export default IntroduceKOL
